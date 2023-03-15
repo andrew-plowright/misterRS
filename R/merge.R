10997dd94a5418fd879637e13d18871b6572ee58
Andrew Plowright <andrew.plowright@alumni.ubc.ca>
2023-03-14 18:59:38 -0700

    Add 'merge()' function

---
+++ R/merge.R
@@ -0,0 +1,104 @@
+#' Merge multiple RSDS into a single one according to zones
+#'
+#' @export
+
+merge <- function(in_rsds, out_rsds, zones_path, zone_attr,
+                    tile_names = NULL, overwrite = FALSE){
+
+
+  process_timer <- .headline("MERGE")
+
+  ### INPUT CHECKS ----
+
+  for(in_rsds_i in in_rsds){
+    .check_complete_input(in_rsds_i,  tile_names)
+    .check_extension(in_rsds_i,  "shp")
+  }
+  .check_extension(out_rsds,  "shp")
+
+  # Get tiles
+  #ts <- .get_tilescheme()
+
+  # Get file paths
+  out_files  <- .get_rsds_tilepaths(out_rsds)
+  in_files <- lapply(in_rsds, function(in_rsds_i){.get_rsds_tilepaths(in_rsds_i)})
+
+  # Read zones
+  zones <- sf::st_read(zones_path, quiet = TRUE)
+
+  if(!all(setdiff(names(in_rsds), "<none>") %in% unique(zones[[zone_attr]]))) stop(
+    "Could match list names of 'in_rsds' to the values in the '", zone_attr,
+    "' attribute of '", basename(zones_path), "'")
+
+  ### CREATE WORKER ----
+
+  tile_worker <-function(tile_name){
+
+
+   # tile_nbuff <- sf::st_as_sf(ts[tile_name][["nbuffs"]])
+
+    # Output file
+    out_file  <- out_files[tile_name]
+
+    # Read in files and merge according to zones
+    in_sps <- lapply(names(in_files), function(zone_name){
+
+      in_file <- in_files[[zone_name]][tile_name]
+
+      in_sp <- sf::st_read(in_file, quiet = TRUE)
+
+      # Intersect with no zone
+      if(zone_name == "<none>"){
+
+        intersec <- sapply(sf::st_intersects(in_sp, zones), function(x) if(length(x) >0) FALSE else TRUE)
+
+      # Intersect with a given zone
+      }else{
+
+        zone <- zones[zones[[zone_attr]] == zone_name, ]
+
+        intersec <- sapply(sf::st_intersects(in_sp, zone), function(x) if(length(x) >0) TRUE else FALSE)
+      }
+
+
+      if(length(intersec) > 0 && any(intersec)){
+        return(in_sp[intersec,])
+      }else{
+        in_sp[c(),]
+      }
+    })
+    out_sp <- do.call(dplyr::bind_rows, in_sps)
+
+    # Intersect with nbuff tile
+    # intersec <- sapply(sf::st_intersects(out_sp, tile_nbuff), function(x) if(length(x) >0) TRUE else FALSE)
+    # if(length(intersec) > 0){
+    #   out_sp <- out_sp[intersec,]
+    # }
+
+    # Return empty
+    sf::st_write(out_sp, out_file, quiet=TRUE, delete_dsn = overwrite)
+
+
+    if(file.exists(out_file)){
+      return("Success")
+    }else{
+      stop("Failed to create tile")
+    }
+  }
+
+
+  ### APPLY WORKER ----
+
+  # Get tiles for processing
+  queued_tiles <- .tile_queue(out_files, overwrite, tile_names)
+
+  # Process
+  process_status <- .exe_tile_worker(queued_tiles, tile_worker)
+
+  # Report
+  .print_process_status(process_status)
+
+  # Conclude
+  .conclusion(process_timer)
+
+}
