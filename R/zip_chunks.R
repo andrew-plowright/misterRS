f1c32ca258874403fcc012aa662cc3102e173a3b
Andrew Plowright <andrew.plowright@alumni.ubc.ca>
2023-03-22 18:14:43 -0700

    Update 'zip_chunks()'

--- R/zip_chunks.R
+++ R/zip_chunks.R
@@ -2,13 +2,24 @@
 #'
 #' @export
 
-zip_chunks <- function(rsds, out_dir, prefix, chunk_size = 100){
+zip_chunks <- function(in_rsds, out_dir, prefix, chunk_size = 100){
 
   out_dir <- tools::file_path_as_absolute(out_dir)
 
-  chunks <- split(rsds@tilePaths, ceiling(seq_along(rsds@tilePaths)/chunk_size))
+  tile_paths <- .get_rsds_tilepaths(in_rsds)
 
-  for(i in 1:length(chunks)){
+  chunks <- split(tile_paths, ceiling(seq_along(tile_paths)/chunk_size))
+  n_chunks <- length(chunks)
+
+  cat("ZIP CHUNKS", "\n\n",
+      "  Chunk size : ", chunk_size,"\n",
+      "  Chunk no.  : ", n_chunks,
+      "\n\n", sep=""
+      )
+
+  for(i in 1:n_chunks){
+
+    cat("  Zipping chunk", i, "\n", sep="")
 
     out_file <- file.path(out_dir, paste0(prefix, i, ".zip"))
 
@@ -17,4 +28,6 @@ zip_chunks <- function(rsds, out_dir, prefix, chunk_size = 100){
       files   = chunks[[i]],
       extras  = "-j -q")
   }
+
+  cat("\n", "Finished", "\n", sep="")
 }
