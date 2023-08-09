6bc963619644cb61a51ca5887f637501b86006d5
Andrew Plowright <andrew.plowright@alumni.ubc.ca>
2023-08-08 17:47:14 -0700

    add support for VRTs to 'to_rsds()'

--- R/to_rsds.R
+++ R/to_rsds.R
@@ -9,26 +9,12 @@ to_rsds <- function(in_files, out_rsds, res, bands = NULL,
 
   ### CHECK INPUTS ----
 
+  # Check input file existence
   if(!all(file.exists(in_files))) stop("Some input files were missing")
 
-  # Set file paths
-  temp_list <- tempfile(fileext = ".txt")
-  temp_vrt  <- tempfile(fileext = ".vrt")
-
-  # Write list of input files to a text file
-  write(in_files, temp_list)
-
-  # Format bands argument
-  bands <- if(!is.null(bands)) as.list(setNames(bands, rep("b", length(bands))))
-
-  # List of VRT arguments
-  # NOTE: 'srcnodata = "None"' prevents No Data values from being included
-  arg_list <- c(list(input_file_list = temp_list), bands, list(srcnodata = "None"), temp_vrt)
-
-  # Generate VRT
-  do.call(gpal2::gdalbuildvrt, arg_list)
-
-  if(!file.exists(temp_vrt)) stop("Failed to create VRT")
+  # Get file extension
+  in_ext <- tolower(unique(tools::file_ext(unique(in_files))))
+  if(length(in_ext) > 1) stop("Cannot use multiple file extensions")
 
   # Get tiles
   ts <- .get_tilescheme()
@@ -36,6 +22,35 @@ to_rsds <- function(in_files, out_rsds, res, bands = NULL,
 
   # Get tile names
   out_files <- .get_rsds_tilepaths(out_rsds)
+
+  if(in_ext == "tif"){
+
+    # Set file paths
+    temp_list <- tempfile(fileext = ".txt")
+    temp_vrt  <- tempfile(fileext = ".vrt")
+
+    # Write list of input files to a text file
+    write(in_files, temp_list)
+
+    # Format bands argument
+    bands <- if(!is.null(bands)) as.list(setNames(bands, rep("b", length(bands))))
+
+    # List of VRT arguments
+    # NOTE: 'srcnodata = "None"' prevents No Data values from being included
+    arg_list <- c(list(input_file_list = temp_list), bands, list(srcnodata = "None"), temp_vrt)
+
+    # Generate VRT
+    do.call(gpal2::gdalbuildvrt, arg_list)
+
+    if(!file.exists(temp_vrt)) stop("Failed to create VRT")
+
+  }else if(in_ext == "vrt"){
+
+    if(length(in_files) > 1) stop("It may be possible to use multiple VRTs, but this hasn't been tested yet")
+
+    temp_vrt <- in_files
+
+  }else stop("Unusable file extension: '", in_ext, "'")
 
 
   ### CREATE WORKER ----
